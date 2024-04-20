92f4967fc989038925b6589dd03f2b4ca2617a87
erynder-z <stfn.bgr@gmail.com>
2024-04-20 18:45:57 +0200

    Display damage messages

--- src/components/Commands/HealthAdjust.ts
+++ src/components/Commands/HealthAdjust.ts
@@ -26,11 +26,11 @@ export class HealthAdjust {
    * @param {number} amount - The amount of health to be healed.
    */
   public static heal(mob: Mob, amount: number) {
-    console.log(`heal ${mob.hp} + ${amount}`);
+    /*  console.log(`heal ${mob.hp} + ${amount}`); */
     const limit = mob.maxhp - mob.hp;
     if (amount > limit) amount = limit;
     mob.hp += amount;
-    console.log(`heal ${mob.hp}`);
+    /*   console.log(`heal ${mob.hp}`); */
   }
 
   /**
@@ -42,14 +42,17 @@ export class HealthAdjust {
    */
   static damage(mob: Mob, amount: number, game: GameIF, attacker: Mob | null) {
     AutoHeal.combatResets(mob, attacker, game);
-    console.log('damage', amount, mob.hp);
+    /*  console.log('damage', amount, mob.hp); */
     mob.hp -= amount;
-    console.log('damage to', amount, mob.hp);
-    if (mob.hp <= 0) {
-      const involvesPlayer =
-        mob.isPlayer || (attacker !== null && attacker.isPlayer);
-      this.mobDies(mob, game, involvesPlayer);
-    }
+    /*     console.log('damage to', amount, mob.hp); */
+
+    const msg = this.generateDamageMessage(mob, amount);
+    if (mob.isPlayer) game.flash(msg);
+
+    const involvesPlayer =
+      mob.isPlayer || (attacker !== null && attacker.isPlayer);
+
+    if (mob.hp <= 0) this.mobDies(mob, game, involvesPlayer);
   }
 
   /**
@@ -59,8 +62,39 @@ export class HealthAdjust {
    */
   static mobDies(mob: Mob, game: GameIF, involvesPlayer: boolean) {
     const s = `${mob.name} dies.`;
-    if (involvesPlayer) game.message(s);
+    if (involvesPlayer) {
+      game.message(s);
+      game.flash(s);
+    }
     const map = <MapIF>game.currentMap();
     map.removeMob(mob);
+  }
+
+  /**
+   * Generates a message based on the damage percentage inflicted on the mob.
+   *
+   * @param {Mob} mob - The mob object taking the damage.
+   * @param {number} amount - The amount of damage inflicted.
+   * @return {string} The message describing the level of damage.
+   */
+  static generateDamageMessage(mob: Mob, amount: number): string {
+    const damagePercentage = Math.round((amount / mob.maxhp) * 100);
+    let message = '';
+
+    if (damagePercentage >= 100) {
+      message = `Everything around you begins to fade...`;
+    } else if (damagePercentage >= 75) {
+      message = `You are almost being taken out by a single blow! Run!`;
+    } else if (damagePercentage >= 50) {
+      message = `You take some major damage! This may end badly...`;
+    } else if (damagePercentage >= 25) {
+      message = `You receive a major blow!`;
+    } else if (damagePercentage >= 10) {
+      message = `You are feeling some pain!`;
+    } else {
+      message = `You take a little bit of damage.`;
+    }
+
+    return message;
   }
 }
