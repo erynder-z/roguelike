d5638edd9be80877f62f3204ffac549a2506ba71
erynder-z <stfn.bgr@gmail.com>
2024-05-26 15:55:56 +0200

    Fix VisibilityAwareSleepAI

--- src/components/Mobs/VisibilityAwareSleepAI.ts
+++ src/components/Mobs/VisibilityAwareSleepAI.ts
@@ -25,7 +25,7 @@ export class VisibilityAwareSleepAI implements MobAI {
     if (!VisibilityAwareSleepAI.isNear(me, enemy)) return true;
     const map = <GameMap>game.currentMap();
     const canSee = CanSee.checkMobLOS_Bresenham(me, enemy, map, true);
-    if (!CanSee) return true;
+    if (!canSee) return true;
 
     me.mood = game.rand.isOneIn(3) ? Mood.Awake : Mood.Asleep;
     return true;
