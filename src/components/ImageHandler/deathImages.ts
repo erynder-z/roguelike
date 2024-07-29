78becd0d3af883a1266031da79a4f2e6536ad9ff
erynder-z <stfn.bgr@gmail.com>
2024-07-29 22:39:43 +0200

    Add player death images

---
+++ src/components/ImageHandler/deathImages.ts
@@ -0,0 +1,8 @@
+import death01 from '../../assets/images/death/death01.webp';
+import death02 from '../../assets/images/death/death02.webp';
+import death03 from '../../assets/images/death/death03.webp';
+import death04 from '../../assets/images/death/death04.webp';
+
+const deathImages = [death01, death02, death03, death04];
+
+export default deathImages;
