907ebf4b41d5d1d48118d893721d51b536555b98
erynder-z <stfn.bgr@gmail.com>
2024-08-06 22:07:48 +0200

    Update gameover screen

--- src/components/Screens/GameOverScreen.ts
+++ src/components/Screens/GameOverScreen.ts
@@ -1,4 +1,3 @@
-import { DrawableTerminal } from '../Terminal/Types/DrawableTerminal';
 import { Stack } from '../Terminal/Types/Stack';
 import { StackScreen } from '../Terminal/Types/StackScreen';
 import { ScreenMaker } from './Types/ScreenMaker';
@@ -11,27 +10,118 @@ export class GameOverScreen implements StackScreen {
   constructor(public make: ScreenMaker) {}
 
   /**
-   * Draws the content of the game over screen on the provided drawable terminal.
+   * Draws the game over screen if it hasn't already been drawn.
    *
-   * @param {DrawableTerminal} term - The terminal on which the game over screen content is drawn.
+   * @return {void} This function does not return anything.
    */
-  public drawScreen(term: DrawableTerminal): void {
-    term.drawText(1, 1, 'Game Over', 'yellow', 'red');
+  public drawScreen(): void {
+    const container = document.getElementById(
+      'canvas-container',
+    ) as HTMLDivElement;
+    if (container.querySelector('#gameover-screen')) return;
+
+    const gameOverScreen = this.createGameOverScreen();
+    container.appendChild(gameOverScreen);
   }
 
+  /**
+   * Creates a game over screen element using a DocumentFragment.
+   *
+   * @return {HTMLDivElement} The created game over screen element.
+   */
+  private createGameOverScreen(): HTMLDivElement {
+    const gameOverScreen = document.createElement('div');
+    gameOverScreen.id = 'gameover-screen';
+    gameOverScreen.classList.add('gameover-screen', 'fade-in');
+
+    const fragment = document.createDocumentFragment();
+    const heading = this.createHeading();
+    const messageList = this.createPostMortem();
+    const info = this.createInfo();
+
+    fragment.appendChild(heading);
+    fragment.appendChild(messageList);
+    fragment.appendChild(info);
+    gameOverScreen.appendChild(fragment);
+
+    return gameOverScreen;
+  }
+
+  /**
+   * Creates a new HTML heading element.
+   *
+   * @return {HTMLHeadingElement} The newly created heading element.
+   */
+  private createHeading(): HTMLHeadingElement {
+    const heading = document.createElement('h1');
+    heading.textContent = 'Game Over';
+    return heading;
+  }
+
+  /**
+   * Creates a HTMLDivElement for the post-mortem content.
+   *
+   * @return {HTMLDivElement} The HTMLDivElement containing the post-mortem content.
+   */
+  private createPostMortem(): HTMLDivElement {
+    const postMortem = document.createElement('div');
+    /*   const content = PostMorgemGenerator.generate(); */
+
+    const content = document.createElement('p');
+    content.classList.add('post-mortem-text');
+    content.textContent =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+    postMortem.appendChild(content);
+    return postMortem;
+  }
+
+  /**
+   * Creates an HTMLDivElement element.
+   *
+   * @return {HTMLDivElement} The created HTMLDivElement element.
+   */
+  private createInfo(): HTMLDivElement {
+    const info = document.createElement('div');
+    info.classList.add('gameover-info');
+    info.textContent = 'Press escape to return to title screen.';
+    return info;
+  }
+
+  /**
+   * Removes the game over screen from the document.
+   *
+   * @return {void} This function does not return anything.
+   */
+  private removeGameOverScreen(): void {
+    const gameOverScreen = document.getElementById('gameover-screen');
+    if (gameOverScreen) {
+      gameOverScreen.remove();
+    }
+  }
+
+  /**
+   * Determines if the screen should be updated based on time.
+   *
+   * @param {Stack} stack - The stack of screens.
+   * @return {boolean} Returns `true` if the screen should be updated, `false` otherwise.
+   */
   public onTime(): boolean {
     return false;
   }
 
   /**
-   * Handles keyboard events specific to the game over screen.
-   * Pops the current screen from the stack and pushes a new game screen.
+   * Handles a key down event, checking if the escape key is pressed and performing actions accordingly.
    *
-   * @param {KeyboardEvent} event - The keyboard event to be handled.
-   * @param {Stack} stack - The stack to which the game over screen belongs.
+   * @param {KeyboardEvent} event - The keyboard event to handle.
+   * @param {Stack} stack - The current stack of screens.
+   * @return {void}
    */
   public handleKeyDownEvent(event: KeyboardEvent, stack: Stack): void {
-    stack.pop();
-    stack.push(this.make.newGame());
+    if (event.key === 'Escape') {
+      this.removeGameOverScreen();
+      stack.pop();
+      this.make.titleScreen();
+    }
   }
 }
--- src/components/Screens/Types/ScreenMaker.ts
+++ src/components/Screens/Types/ScreenMaker.ts
@@ -3,6 +3,7 @@ import { StackScreen } from '../../Terminal/Types/StackScreen';
 
 export type ScreenMaker = {
   newGame(): StackScreen;
+  titleScreen(): void;
   gameOver(): StackScreen;
   more(game: GameState | null): StackScreen;
 };
