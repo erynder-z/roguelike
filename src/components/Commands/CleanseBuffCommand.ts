1895069b26b46446114c22250841d3a964511100
erynder-z <stfn.bgr@gmail.com>
2024-08-21 21:54:16 +0200

    Only display buff cleanses if there is a LOS to the mob.

--- src/components/Commands/CleanseBuffCommand.ts
+++ src/components/Commands/CleanseBuffCommand.ts
@@ -3,6 +3,8 @@ import { CommandBase } from './CommandBase';
 import { EventCategory, LogMessage } from '../Messages/LogMessage';
 import { GameState } from '../Builder/Types/GameState';
 import { Mob } from '../Mobs/Mob';
+import { GameMap } from '../MapModel/GameMap';
+import { CanSee } from '../Utilities/CanSee';
 
 /**
  * Represents a command that removes a given buff from the given mob.
@@ -23,16 +25,42 @@ export class CleanseBuffCommand extends CommandBase {
    */
   public execute(): boolean {
     const { me, game } = this;
+    const { player } = game;
 
     if (this.buff) this.me.buffs.cleanse(this.buff, game, me);
 
-    const msg = new LogMessage(
-      `Cleansed ${Buff[this.buff]}!`,
-      EventCategory.heal,
-    );
+    const shouldDisplayMessage = this.shouldDisplayMessage(game, me, player);
 
-    game.message(msg);
+    if (shouldDisplayMessage) {
+      const s = me.isPlayer ? 'You are' : `${me.name} is`;
+      const msg = new LogMessage(
+        `${s} cleansed of ${Buff[this.buff]}!`,
+        EventCategory.heal,
+      );
+
+      game.message(msg);
+    }
 
     return true;
+  }
+
+  /**
+   * Determines whether a message should be displayed based on the visibility of the mob to the player.
+   *
+   * @param {GameState} game - The current game state.
+   * @param {Mob} mob - The mob for which the message is being considered.
+   * @param {Mob} player - The player mob.
+   * @return {boolean} True if the message should be displayed, false otherwise.
+   */
+  private shouldDisplayMessage(
+    game: GameState,
+    mob: Mob,
+    player: Mob,
+  ): boolean {
+    const map = <GameMap>game.currentMap();
+
+    const iVisible = CanSee.checkMobLOS_Bresenham(mob, player, map, false);
+
+    return iVisible;
   }
 }
