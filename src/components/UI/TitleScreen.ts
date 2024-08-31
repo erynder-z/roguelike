d5571e0dd6524ded8655b8df2394c9d92e7ae353
erynder-z <stfn.bgr@gmail.com>
2024-08-31 17:01:23 +0200

    Add version display

--- src/components/UI/TitleScreen.ts
+++ src/components/UI/TitleScreen.ts
@@ -13,107 +13,135 @@ export class TitleScreen extends HTMLElement {
 
     const templateElement = document.createElement('template');
     templateElement.innerHTML = `
-        <style>
-          .title-screen {
-            font-family: 'UASQUARE';
-            font-size: 2.5rem;
-            position: absolute;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: start;
-            height: 100%;
-            width: 100%;        
-            background: var(--background1);
-            color: var(--white);
-            z-index: 1;
-            overflow: hidden;
-          }
-          .background-image {
-            position: absolute;
-            top: 0;
-            left: 0;
-            height: 100%;
-            width: 100%;
-            background-image: url('${img}');
-            background-size: cover; /* Maintain aspect ratio */
+      <style>
+        .title-screen {
+          font-family: 'UASQUARE';
+          font-size: 2.5rem;
+          position: absolute;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: start;
+          height: 100%;
+          width: 100%;
+          background: var(--background1);
+          color: var(--white);
+          z-index: 1;
+          overflow: hidden;
+        }
+
+        .background-image {
+          position: absolute;
+          top: 0;
+          left: 0;
+          height: 100%;
+          width: 100%;
+          background-image: url('${img}');
+          background-size: cover;
+          background-position: left;
+          animation: pan 25s infinite linear;
+          z-index: -1;
+        }
+
+        .title-screen h1 {
+          margin-top: 12rem;
+          text-align: center;
+          z-index: 1;
+        }
+
+        .title-screen button {
+          font-family: 'UASQUARE';
+          padding: 1rem;
+          font-size: 2.5rem;
+          font-weight: bold;
+          background: none;
+          color: var(--white);
+          border: none;
+          transition: all 0.2s ease-in-out;
+        }
+
+        .title-screen button:hover {
+          cursor: pointer;
+          transform: scale(1.1);
+        }
+
+        .underline {
+          text-decoration: underline;
+        }
+
+        .buttons-container {
+          position: absolute;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          height: 100%;
+          gap: 0.5rem;
+        }
+
+        .bottom-container {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          width: 100%;
+          margin-top: auto;
+        }
+
+        .seed-display {
+          padding: 0 1rem;
+          font-size: 1.5rem;
+        }
+
+        .seed-display span {
+          font-size: 1.75rem;
+          font-weight: bold;
+        }
+
+        .version-display {
+          padding: 0 1rem;
+          font-size: 1.5rem;
+        }
+
+        @keyframes pan {
+          0% {
             background-position: left;
-            animation: pan 25s infinite linear;
-            z-index: -1;
           }
-          .title-screen h1 {
-            margin-top: 12rem;  
-            text-align: center;
-            z-index: 1;
+          50% {
+            background-position: right;
           }
-          .title-screen button {
-            font-family: 'UASQUARE';
-            padding: 1rem;
-            font-size: 2.5rem;
-            font-weight: bold;
-            background: none;
-            color: var(--white);
-            border: none;
-            transition: all 0.2s ease-in-out;
+          100% {
+            background-position: left;
           }
+        }
+      </style>
 
-          .title-screen button:hover {
-           cursor: pointer;
-           transform: scale(1.1);
-          }
-
-          .underline {
-            text-decoration: underline;
-          }
-
-          .buttons-container {
-            position: absolute;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            height: 100%;
-            gap: 0.5rem;
-          }
-
-          .seed-display {
-            position: absolute;
-            bottom: 1rem; 
-            right: 1rem; 
-            font-size: 1.5rem; 
-          
-          }
-
-          .seed-display span {
-             font-size: 1.75rem; 
-            font-weight: bold;
-          }
-
-          @keyframes pan {
-            0% {
-              background-position: left;
-            }
-            50% {
-              background-position: right;
-            }
-            100% {
-              background-position: left;
-            }
-          }
-        </style>
-  
-        <div class="title-screen">
-          <div class="background-image"></div>
-          <h1>Meikai: Roguelike Journey to the Center of the Earth</h1>
-          <div class="buttons-container">
-            <button id="new-game-button"><span class="underline">N</span>ew Game</button>
-            <button id="change-seed-button"><span class="underline">C</span>hange seed</button>
-            <button id="help-button"><span class="underline">H</span>elp</button>
-            <button id="about-window-button"><span class="underline">A</span>bout</button>
-            <button id="quit-window-button"><span class="underline">Q</span>uit</button>
-          </div>
-            <div id="current-seed-display" class="seed-display">current Seed</div>
+      <div class="title-screen">
+        <div class="background-image"></div>
+        <h1>Meikai: Roguelike Journey to the Center of the Earth</h1>
+        <div class="buttons-container">
+          <button id="new-game-button">
+            <span class="underline">N</span>ew Game
+          </button>
+          <button id="change-seed-button">
+            <span class="underline">C</span>hange seed
+          </button>
+          <button id="help-button">
+            <span class="underline">H</span>elp
+          </button>
+          <button id="about-window-button">
+            <span class="underline">A</span>bout
+          </button>
+          <button id="quit-window-button">
+            <span class="underline">Q</span>uit
+          </button>
         </div>
-      `;
+        <div class="bottom-container">
+          <div class="version-display">version: alpha</div>
+          <div id="current-seed-display" class="seed-display">
+            current Seed
+          </div>
+        </div>
+      </div>
+    `;
 
     shadowRoot.appendChild(templateElement.content.cloneNode(true));
 
