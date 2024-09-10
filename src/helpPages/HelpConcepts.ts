c3a625d6bd764d90f31eb12dfa6ceea7ae079959
erynder-z <stfn.bgr@gmail.com>
2024-09-10 21:02:40 +0200

    Add help-concepts content

--- src/helpPages/HelpConcepts.ts
+++ src/helpPages/HelpConcepts.ts
@@ -1,3 +1,5 @@
+import conceptsData from './concepts.json';
+
 export class HelpConcepts extends HTMLElement {
   constructor() {
     super();
@@ -23,16 +25,61 @@ export class HelpConcepts extends HTMLElement {
           background-color: var(--selection-background);
         }
 
-        h1 {
-          margin: 0;
-          text-align: center;
+        .concepts-heading {
+           margin-top: 1rem;
+           width: 100%;
+           background-color: var(--whiteTransparent);
+           padding: 0.75rem;
+           text-align: left;
+           font-weight: bold;
+           border-bottom: 1px solid var(--whiteTransparent);
+        }
+
+        .concepts-content {
+           font: 1rem DejaVu Sans Mono, monospace;
+           padding: 0.75rem;
+           text-align: left;
+           font-style: italic;
         }
       </style>
 
-      <h1>Game concepts</h1>
-      <p>Information about various concepts.</p>
+      <div id="concents-list">
+        <!-- Buffs will be mapped here -->
+      </div>
     `;
 
     shadowRoot.appendChild(templateElement.content.cloneNode(true));
+
+    this.populateConcepts();
+  }
+
+  /**
+   * Populates the list of concepts in the help page.
+   *
+   * This function is called once, when the component is first created.
+   * It reads the concepts data from the JSON file and creates a heading
+   * and paragraph element for each concept, with the concept's name
+   * and description. The heading and paragraph elements are then appended
+   * to the list element.
+   */
+  private populateConcepts(): void {
+    const conceptsListElement = this.shadowRoot?.querySelector(
+      '#concents-list',
+    ) as HTMLDivElement;
+
+    conceptsData.concepts.forEach(
+      (conceptObj: { concept: string; description: string }) => {
+        const conceptHeading = document.createElement('div');
+        conceptHeading.classList.add('concepts-heading');
+        conceptHeading.textContent = conceptObj.concept;
+
+        const conceptContent = document.createElement('p');
+        conceptContent.classList.add('concepts-content');
+        conceptContent.textContent = conceptObj.description;
+
+        conceptsListElement.appendChild(conceptHeading);
+        conceptsListElement.appendChild(conceptContent);
+      },
+    );
   }
 }
