3ac2580cf823b0c7f9ed94b16fe6fc4c80c31bd0
erynder-z <stfn.bgr@gmail.com>
2024-10-27 18:01:26 +0100

    Fix potential wrongly formatted json in config

--- src/utilities/saveConfig.ts
+++ src/utilities/saveConfig.ts
@@ -1,26 +1,28 @@
-import { open, BaseDirectory } from '@tauri-apps/plugin-fs';
+import { open, BaseDirectory, writeTextFile } from '@tauri-apps/plugin-fs';
 import { gameConfig } from '../gameConfig/gameConfig';
 
 /**
- * Saves the current game configuration to a file.
+ * Saves the current game configuration to a JSON file named `gameConfig.json`.
  *
- * The file is saved in the application data directory as `gameConfig.json`.
- * The file is written in UTF-8 format with pretty-printed JSON for readability.
+ * The configuration is saved in the application's data directory with pretty-printed JSON for readability.
+ * If the file does not exist, it is created. The function handles any errors that occur during the file
+ * operation by logging them to the console.
  *
- * @returns {Promise<void>} A promise for when the configuration is saved.
+ * @returns {Promise<void>}
  */
 export const saveConfig = async (): Promise<void> => {
   try {
-    const encoder = new TextEncoder();
     const contents = JSON.stringify(gameConfig, null, 2); // Pretty print for readability
-    const data = encoder.encode(contents);
 
     const file = await open('gameConfig.json', {
       write: true,
       create: true,
       baseDir: BaseDirectory.AppData,
     });
-    await file.write(data);
+
+    await writeTextFile('gameConfig.json', contents, {
+      baseDir: BaseDirectory.AppData,
+    });
     await file.close();
   } catch (error) {
     console.error(error);
