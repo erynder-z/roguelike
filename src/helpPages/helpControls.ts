b79c54b066ea7d291bf6da8c7a5ecc603082b4d4
erynder-z <stfn.bgr@gmail.com>
2024-11-24 17:12:56 +0100

    List controls for all available schemes in help page

--- src/helpPages/helpControls.ts
+++ src/helpPages/helpControls.ts
@@ -1,6 +1,18 @@
 import controlsData from './controls.json';
+import controlSchemes from '../controls/control_schemes.json';
+import {
+  ControlSchemeName,
+  ControlSchemeType,
+} from '../types/controls/controlSchemeType';
+import { gameConfigManager } from '../gameConfigManager/gameConfigManager';
 
 export class HelpControls extends HTMLElement {
+  private gameConfig = gameConfigManager.getConfig();
+  private controlSchemeName: ControlSchemeName = this.gameConfig.control_scheme;
+  private availableControlSchemes = Object.keys(
+    controlSchemes,
+  ) as ControlSchemeName[];
+
   constructor() {
     super();
 
@@ -32,12 +44,29 @@ export class HelpControls extends HTMLElement {
           flex-direction: column;
         }
 
+        .control-scheme-button-container {
+          display: flex;
+          margin-top: 1rem;
+        }
+
+        .control-scheme-button {
+          font-family: 'UASQUARE';
+          padding: 0.5rem 1.25rem;
+          background: none;
+          border: none;
+          color: var(--white);
+          font-size: 1.25rem;
+          cursor: pointer;
+        }
+
+        .control-scheme-button:hover {
+          background-color: var(--whiteTransparent);
+        }
+
         table {
           width: 100%;
           border-collapse: collapse;
           margin-top: 1rem;
-      
-
         }
 
         th, td {
@@ -51,7 +80,7 @@ export class HelpControls extends HTMLElement {
         }
 
         tbody {
-        font: 1rem DejaVu Sans Mono, monospace;
+          font: 1rem DejaVu Sans Mono, monospace;
         }
 
         .key-cell {
@@ -60,12 +89,19 @@ export class HelpControls extends HTMLElement {
 
         .action-cell {
           font-style: italic;
-    
         }
 
+        .underline {
+          text-decoration: underline;
+        }
       </style>
 
       <div class="container">
+        <div class="control-scheme-button-container">
+          <button id="prev-scheme-button" class="control-scheme-button"><span class="underline">&lt;</span></button>
+          <div id="controls-scheme-name" class="control-scheme-name">${this.controlSchemeName.toUpperCase()} - controls</div>
+          <button id="next-scheme-button" class="control-scheme-button"><span class="underline">&gt;</span></button>
+        </div>
         <table>
           <thead>
             <tr>
@@ -74,40 +110,207 @@ export class HelpControls extends HTMLElement {
             </tr>
           </thead>
           <tbody id="controls-list">
-          <!-- Controls will be mapped here -->
           </tbody>
         </table>
       </div>
-    `;
+   `;
 
     shadowRoot.appendChild(templateElement.content.cloneNode(true));
 
-    this.populateControls();
+    this.bindEvents();
+    this.populateControlsTable(this.controlSchemeName);
   }
 
   /**
-   * Populates the controls table with data from controls.json.
+   * Binds events to the elements inside the controls help page.
    *
-   * Loops over the controls data and creates a table row for each control,
-   * with the key in the first column and the action in the second column.
+   * The function binds the following events:
+   * - Previous control scheme button click event
+   * - Next control scheme button click event
+   * - Keydown event on the document
+   *
+   * @return {void}
    */
-  private populateControls() {
-    const buffsListElement = this.shadowRoot?.querySelector(
+  private bindEvents(): void {
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.handleControlSchemeButtonClick =
+      this.handleControlSchemeButtonClick.bind(this);
+
+    this.manageEventListener(
+      'prev-scheme-button',
+      'click',
+      () => this.handleControlSchemeButtonClick('prev'),
+      true,
+    );
+    this.manageEventListener(
+      'next-scheme-button',
+      'click',
+      () => this.handleControlSchemeButtonClick('next'),
+      true,
+    );
+
+    document.addEventListener('keydown', this.handleKeyPress);
+  }
+
+  /**
+   * Manage event listeners for an element.
+   *
+   * If the add parameter is true, the callback is added to the element's event
+   * listeners. If the add parameter is false, the callback is removed from the
+   * element's event listeners.
+   *
+   * @param {string} elementId - The ID of the element on which to add or remove
+   * the event listener.
+   * @param {string} eventType - The type of event to listen for.
+   * @param {EventListener} callback - The callback function to be called when the
+   * event is fired.
+   * @param {boolean} add - Whether to add or remove the event listener.
+   * @return {void}
+   */
+  private manageEventListener(
+    elementId: string,
+    eventType: string,
+    callback: EventListener,
+    add: boolean,
+  ): void {
+    const element = this.shadowRoot?.getElementById(elementId);
+    if (add) {
+      element?.addEventListener(eventType, callback);
+    } else {
+      element?.removeEventListener(eventType, callback);
+    }
+  }
+
+  /**
+   * Handles a click event on the "prev" or "next" control scheme button.
+   *
+   * Updates the {@link controlSchemeName} property to the name of the next or
+   * previous control scheme in the list of available schemes, and updates the
+   * displayed control scheme name on the page and populates the controls table
+   * with the new control scheme.
+   *
+   * @param { 'prev' | 'next' } buttonDirection - The direction of the button
+   * that was clicked.
+   * @return {void}
+   */
+  private handleControlSchemeButtonClick(
+    buttonDirection: 'prev' | 'next',
+  ): void {
+    const schemes = this.availableControlSchemes;
+    const currentIndex = schemes.indexOf(this.controlSchemeName);
+    const newIndex =
+      buttonDirection === 'prev'
+        ? (currentIndex - 1 + schemes.length) % schemes.length
+        : (currentIndex + 1) % schemes.length;
+
+    this.controlSchemeName = schemes[newIndex];
+    this.updateControlSchemeName();
+    this.populateControlsTable(this.controlSchemeName);
+  }
+
+  /**
+   * Updates the displayed control scheme name on the page.
+   *
+   * Selects the element with the ID "controls-scheme-name" and sets its text
+   * content to the uppercased control scheme name followed by " - controls".
+   *
+   * @return {void}
+   */
+  private updateControlSchemeName() {
+    const schemeNameElement = this.shadowRoot?.querySelector(
+      '#controls-scheme-name',
+    ) as HTMLDivElement;
+    schemeNameElement.textContent = `${this.controlSchemeName.toUpperCase()} - controls`;
+  }
+
+  /**
+   * Populates the controls table with the controls of the given control scheme.
+   *
+   * Selects the element with the ID "controls-list" and populates it with a table
+   * row for each control in the given control scheme. The table row contains two
+   * cells: one for the key and one for the description of the control.
+   *
+   * @param {ControlSchemeName} controlSchemeName - The name of the control scheme
+   * to populate the table with.
+   * @return {void}
+   */
+  private populateControlsTable(controlSchemeName: ControlSchemeName): void {
+    const controlsListElement = this.shadowRoot?.querySelector(
       '#controls-list',
     ) as HTMLTableSectionElement;
 
-    controlsData.controls.forEach(controlData => {
-      const rowElement = document.createElement('tr');
-      const keyCellElement = document.createElement('td');
-      const actionCellElement = document.createElement('td');
+    if (!controlsListElement) return;
 
-      keyCellElement.textContent = controlData.key;
-      keyCellElement.classList.add('key-cell');
-      actionCellElement.textContent = controlData.action;
-      actionCellElement.classList.add('action-cell');
+    controlsListElement.innerHTML = '';
 
-      rowElement.append(keyCellElement, actionCellElement);
-      buffsListElement.appendChild(rowElement);
+    const selectedScheme: ControlSchemeType = controlSchemes[controlSchemeName];
+    if (!selectedScheme) return;
+
+    controlsData.controls.forEach(({ action, description }) => {
+      const row = document.createElement('tr');
+      const keyCell = document.createElement('td');
+      const actionCell = document.createElement('td');
+
+      const keys = selectedScheme[action];
+      keyCell.textContent = keys ? keys.join(' / ') : 'N/A';
+      keyCell.classList.add('key-cell');
+
+      actionCell.textContent = description;
+      actionCell.classList.add('action-cell');
+
+      row.append(keyCell, actionCell);
+      controlsListElement.appendChild(row);
     });
+  }
+
+  /**
+   * Handles key presses for navigating control schemes.
+   *
+   * This function responds to key presses of the '<' and '>' keys, interpreting
+   * them as commands to switch to the previous and next control schemes,
+   * respectively. It calls `handleControlSchemeButtonClick` with the appropriate
+   * direction based on the key pressed.
+   *
+   * @param {KeyboardEvent} event - The keyboard event that triggered the function.
+   * @return {void}
+   */
+  private handleKeyPress(event: KeyboardEvent): void {
+    switch (event.key) {
+      case '<':
+        this.handleControlSchemeButtonClick('prev');
+        break;
+      case '>':
+        this.handleControlSchemeButtonClick('next');
+        break;
+      default:
+        break;
+    }
+  }
+
+  /**
+   * Removes event listeners for keydown and click events.
+   *
+   * This function is called when the custom element is removed from the DOM.
+   * It removes event listeners for keydown and click events that were added in the
+   * connectedCallback function.
+   *
+   * @return {void}
+   */
+  disconnectedCallback(): void {
+    const shadowRoot = this.shadowRoot;
+    if (shadowRoot) {
+      shadowRoot
+        .getElementById('switch-controls-button')
+        ?.removeEventListener('click', () =>
+          this.handleControlSchemeButtonClick('prev'),
+        );
+      shadowRoot
+        .getElementById('switch-controls-button')
+        ?.removeEventListener('click', () =>
+          this.handleControlSchemeButtonClick('next'),
+        );
+
+      document.removeEventListener('keydown', this.handleKeyPress);
+    }
   }
 }
