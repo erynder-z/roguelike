20d6e7308321d4364b283016ca3045983f66e998
erynder-z <stfn.bgr@gmail.com>
2025-01-24 19:28:03 +0100

    Display mob health as message instead of a number

--- src/ui/entityInfoDisplay/entityInfoCard.ts
+++ src/ui/entityInfoDisplay/entityInfoCard.ts
@@ -59,12 +59,28 @@ export class EntityInfoCard extends HTMLElement {
         .corpse-description,
         .item-description,
         .env-description {
-          margin: 0;
+          margin: 0 0 0.5rem 0;
           font-size: 1rem;
         }
 
-        .mob-level, .mob-hp, .item-level, .item-spell, .item-charges {
+        .mob-level,
+        .mob-hp,
+        .item-level,
+        .item-spell,
+        .item-charges {
           margin: 0 0 0.5rem 0;
+        }
+
+        .red-hp {
+          color: red;
+        }
+
+        .orange-hp {
+          color: orange;
+        }
+        
+        .yellow-hp {
+          color: yellow;
         }
 
         @keyframes fade-in {
@@ -95,6 +111,11 @@ export class EntityInfoCard extends HTMLElement {
     shadowRoot?.appendChild(templateElement.content.cloneNode(true));
   }
 
+  /**
+   * Fills the entity card with the given entity information.
+   *
+   * @param entity - The entity to fill the card with.
+   */
   public fillCardDetails(entity: LookScreenEntity): void {
     const entityCard = this.shadowRoot?.querySelector('.entity-card');
     if (!entityCard) return;
@@ -107,8 +128,9 @@ export class EntityInfoCard extends HTMLElement {
     const glyphColor = glyphInfo.fgCol;
     const description = entity.description;
     const level = entity.level;
-    const hp = entity.hp;
-    const maxHp = entity.maxHp;
+    const hp = entity.hp || 0;
+    const maxHp = entity.maxHp || 0;
+    const mobHealthMessage = this.getMobHealthMessage(hp, maxHp);
     const charges = entity.charges;
     const spell = entity.spell !== undefined ? Spell[entity.spell] : undefined;
     const envEffects = entity?.envEffects?.map(effect => EnvEffect[effect]);
@@ -122,8 +144,8 @@ export class EntityInfoCard extends HTMLElement {
             <div class="mob-title">${name}</div>
             <div class="mob-glyph" style="color: ${glyphColor}">${glyphChar}</div>
             <div class="mob-level">Mob level: ${level}</div>
-            <div class="mob-hp">HP: ${hp}/${maxHp}</div>
             <div class="mob-description">${description}</div>
+            <div class="mob-hp">This ${name} looks ${mobHealthMessage}.</div>
           `;
         break;
 
@@ -164,6 +186,36 @@ export class EntityInfoCard extends HTMLElement {
         break;
     }
   }
+
+  /**
+   * Gets a string describing the health state of a mob, as a proportion of its
+   * current HP to its max HP.
+   *
+   * @param {number} currentHP - The current HP of the mob.
+   * @param {number} maxHP - The maximum HP of the mob.
+   * @return {string} An HTML string describing the health state of the mob.
+   */
+  private getMobHealthMessage(currentHP: number, maxHP: number): string {
+    const hpPercentage = (currentHP / maxHP) * 100;
+    if (hpPercentage <= 25) {
+      return `<span class="red-hp">almost dead</span>`;
+    } else if (hpPercentage <= 50) {
+      return `<span class="orange-hp">severely injured</span>`;
+    } else if (hpPercentage <= 75) {
+      return `<span class="yellow-hp">slightly wounded</span>`;
+    } else {
+      return `<span>unharmed</span>`;
+    }
+  }
+
+/**
+ * Triggers a fade-out animation on the entity card and removes it from the DOM
+ * once the animation is completed.
+ * 
+ * This method adds the 'fade-out' class to the entity card element, initiating
+ * a CSS animation. It listens for the 'animationend' event to remove the 
+ * element from the DOM, ensuring the animation completes before removal.
+ */
 
   public fadeOutAndRemove(): void {
     const entityCard = this.shadowRoot?.querySelector('.entity-card');
